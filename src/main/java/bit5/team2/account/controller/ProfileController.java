ed482513a925da4089e17901897ad36e0af3ae83
brian reynald <brian_reynald2507@yahoo.com>
2019-06-11 17:05:13 +0700

    Change profile controller - edit success "ok" into null - edit requestMapping and PostMapping

--- src/main/java/bit5/team2/account/controller/ProfileController.java
+++ src/main/java/bit5/team2/account/controller/ProfileController.java
@@ -16,19 +16,19 @@ import bit5.team2.account.model.input.ChangeProfileInput;
 import bit5.team2.account.service.ProfileService;
 
 @RestController
-@RequestMapping("/profile")
+@RequestMapping("/account")
 public class ProfileController extends BaseController{
 	@Autowired
 	ProfileService profileService;
 	
-	@PostMapping(value = "/register-step-1", consumes = "application/json")
+	@PostMapping(value = "/change-profile", consumes = "application/json")
     public @ResponseBody
     ResultEntity<Object> changeProfile(@RequestBody @Valid ChangeProfileInput input, BindingResult bindingResult){
         ResultEntity<Object> errorInput = this.validateInput(bindingResult);
         if (errorInput == null) {
         	int output = profileService.changeProfile(input);
         	if (output == 0)
-        		return this.success("ok");
+        		return this.success(null);
         	else
         		return this.failed();
         }
