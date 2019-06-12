26672d5f82adb23a19ab5e5d9994c5f7e4d28ae9
brian reynald <brian_reynald2507@yahoo.com>
2019-06-12 16:01:50 +0700

    Change Register Service : - make into one single service - change baseController (add another failed) - delete boolean isFinished on user entity

--- src/main/java/bit5/team2/account/controller/RegisterController.java
+++ src/main/java/bit5/team2/account/controller/RegisterController.java
@@ -1,10 +1,9 @@
 package bit5.team2.account.controller;
 
 import bit5.team2.account.lib.BaseController;
+
 import bit5.team2.account.lib.ResultEntity;
-import bit5.team2.account.model.input.InRegister1;
-import bit5.team2.account.model.input.InRegister2;
-import bit5.team2.account.model.input.InRegister3;
+import bit5.team2.account.model.input.InRegister;
 import bit5.team2.account.service.RegisterService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,52 +17,91 @@ import javax.validation.Valid;
 public class RegisterController extends BaseController {
     @Autowired
     RegisterService registerService;
+    
+    @PostMapping(value = "/register", consumes = "application/json")
+    public @ResponseBody
+    ResultEntity<Object> register(@RequestBody @Valid InRegister input, BindingResult bindingResult){
+        ResultEntity<Object> errorInput = this.validateInput(bindingResult);
+        if (errorInput == null) {
+        	int output = registerService.register(input);
+        
+        	if (output == 0) {
+        		return this.success(null);
+        	} else if ( output == 1 ) {
+        		return this.failed("password");
+        	} else if ( output == 2 ) {
+        		return this.failed("email, phone number, username already taken");
+        	} else if ( output == 3 ) {
+        		return this.failed("email, phone number already taken");
+        	} else if ( output == 4 ) {
+        		return this.failed("email, username already taken");
+        	} else if ( output == 5 ) {
+        		return this.failed("phone number, username already taken");
+        	} else if ( output == 6 ) {
+        		return this.failed("email already taken");
+        	} else if ( output == 7 ) {
+        		return this.failed("phone number already taken");
+        	} else if ( output == 8 ) {
+        		return this.failed("username already taken");
+        	} else if ( output == 9 ) {
+        		return this.failed("Phone number already verified");
+        	} else if ( output == 10 ) {
+        		return this.failed("Email already verified");
+        	}
+        	return this.success(null);
+        }
+        else {
+            return errorInput;
+        }
+    }
 
-    @PostMapping(value = "/register-step-1", consumes = "application/json")
-    public @ResponseBody
-    ResultEntity<Object> registerStep1(@RequestBody @Valid InRegister1 input, BindingResult bindingResult){
-        ResultEntity<Object> errorInput = this.validateInput(bindingResult);
-        if (errorInput == null) {
-        	String output = registerService.registerStep1(input);
-        	if (output == null) {
-                return this.failed();
-            } else {
-                return this.success(output);
-            }
-        }
-        else {
-            return errorInput;
-        }
-    }
     
-    @PostMapping(value = "/register-step-2", consumes = "application/json")
-    public @ResponseBody
-    ResultEntity<Object> registerStep2(@RequestBody @Valid InRegister2 input, BindingResult bindingResult){
-        ResultEntity<Object> errorInput = this.validateInput(bindingResult);
-        if (errorInput == null) {
-        	String output = new String();
-        	output = registerService.registerStep2(input);
-        	if (output == null) {
-                return this.failed();
-            } else {
-                return this.success(output);
-            }
-        }
-        else {
-            return errorInput;
-        }
-    }
-    
-    @PostMapping(value = "/register-step-3", consumes = "application/json")
-    public @ResponseBody
-    ResultEntity<Object> registerStep3(@RequestBody @Valid InRegister3 input, BindingResult bindingResult){
-        ResultEntity<Object> errorInput = this.validateInput(bindingResult);
-        if (errorInput == null) {
-        	registerService.registerStep3(input);
-            return this.success(null);
-        }
-        else {
-            return errorInput;
-        }
-    }
 }
+
+//@PostMapping(value = "/register-step-1", consumes = "application/json")
+//public @ResponseBody
+//ResultEntity<Object> registerStep1(@RequestBody @Valid InRegister1 input, BindingResult bindingResult){
+//    ResultEntity<Object> errorInput = this.validateInput(bindingResult);
+//    if (errorInput == null) {
+//    	String output = registerService.registerStep1(input);
+//    	if (output == null) {
+//            return this.failed();
+//        } else {
+//            return this.success(output);
+//        }
+//    }
+//    else {
+//        return errorInput;
+//    }
+//}
+//
+//@PostMapping(value = "/register-step-2", consumes = "application/json")
+//public @ResponseBody
+//ResultEntity<Object> registerStep2(@RequestBody @Valid InRegister2 input, BindingResult bindingResult){
+//    ResultEntity<Object> errorInput = this.validateInput(bindingResult);
+//    if (errorInput == null) {
+//    	String output = new String();
+//    	output = registerService.registerStep2(input);
+//    	if (output == null) {
+//            return this.failed();
+//        } else {
+//            return this.success(output);
+//        }
+//    }
+//    else {
+//        return errorInput;
+//    }
+//}
+//
+//@PostMapping(value = "/register-step-3", consumes = "application/json")
+//public @ResponseBody
+//ResultEntity<Object> registerStep3(@RequestBody @Valid InRegister3 input, BindingResult bindingResult){
+//    ResultEntity<Object> errorInput = this.validateInput(bindingResult);
+//    if (errorInput == null) {
+//    	registerService.registerStep3(input);
+//        return this.success(null);
+//    }
+//    else {
+//        return errorInput;
+//    }
+//}
---
+++ src/main/java/bit5/team2/account/model/input/InRegister.java
@@ -0,0 +1,32 @@
+package bit5.team2.account.model.input;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class InRegister {
+	@Email
+	@NotEmpty
+	private String email;
+
+	@NotEmpty
+	@Pattern(regexp = "(^$|[0-9]{9,12})")
+	@Size(min = 9, max = 12)
+	private String phoneNumber;
+
+    @NotNull
+	private boolean oa;
+    
+    @NotEmpty
+	private String username;
+
+	@NotEmpty
+	private String password;
+}
