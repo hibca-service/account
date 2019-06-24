060d350927fa26829451b33079c202304e2c0ca0
Gede Wayan Dharmawan <gd.wyn.dharmawan@gmail.com>
2019-06-24 14:48:50 +0700

    add entity scan

--- src/main/java/bit5/team2/account/AccountApplication.java
+++ src/main/java/bit5/team2/account/AccountApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"bit5.team2.library.entity"})
+@EntityScan(basePackages = {"bit5.team2.library.entity","bit5.team2.library.view"})
 public class AccountApplication {
 
 	public static void main(String[] args) {
